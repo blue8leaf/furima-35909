7881f47dec0aec0c289653639779f4b47203cd17
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-08-28 14:17:34 +0900

    rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
-    family_name_kanji {Faker::Name.first_name}
-    first_name_kanji {Faker::Name.last_name}
-    family_name_katakana {Faker::Name.first_name}
-    first_name_katakana {Faker::Name.last_name}
-    birthday {Faker::Date.birthday}
+    password { password }
+    password_confirmation { password }
+    family_name_kanji { Faker::Name.first_name }
+    first_name_kanji { Faker::Name.last_name }
+    family_name_katakana { Faker::Name.first_name }
+    first_name_katakana { Faker::Name.last_name }
+    birthday { Faker::Date.birthday }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nickname、email、password、password_confirmation、family_name_kanji、first_name_kanji、family_name_katakana、first_name_katakana、birthdayが存在すれば登録できる" do
+      it 'nickname、email、password、password_confirmation、family_name_kanji、first_name_kanji、family_name_katakana、first_name_katakana、birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
 
@@ -19,13 +19,13 @@ RSpec.describe User do
     end
 
     context '新規登録できない場合' do
-      it "nicknameが空では登録できない" do
+      it 'nicknameが空では登録できない' do
         @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
 
-      it "emaiが空では登録できない" do
+      it 'emaiが空では登録できない' do
         @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
@@ -41,10 +41,10 @@ RSpec.describe User do
       it 'emailに@が含まれない場合登録できない' do
         @user.email = 'test'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
 
-      it "passwordが空では登録できない" do
+      it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
@@ -70,31 +70,31 @@ RSpec.describe User do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
-      it "family_name_kanjiが空では登録できない" do
+      it 'family_name_kanjiが空では登録できない' do
         @user.family_name_kanji = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name kanji can't be blank")
       end
 
-      it "first_name_kanjiが空では登録できない" do
+      it 'first_name_kanjiが空では登録できない' do
         @user.first_name_kanji = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name kanji can't be blank")
       end
 
-      it "family_name_katakanaが空では登録できない" do
+      it 'family_name_katakanaが空では登録できない' do
         @user.family_name_katakana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Family name katakana can't be blank")
       end
 
-      it "first_name_katakanaが空では登録できない" do
+      it 'first_name_katakanaが空では登録できない' do
         @user.first_name_katakana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name katakana can't be blank")
       end
 
-      it "birthdayが空では登録できない" do
+      it 'birthdayが空では登録できない' do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
