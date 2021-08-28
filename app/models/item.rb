872c15045c43d2d2c907bb1eab0b491ea4e1500d
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-08-28 20:53:16 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name,         presence: true
+  validates :explanation,       presence: true
+  validates :category_id,       presence: true
+  validates :status_id,         presence: true
+  validates :delivery_price_id, presence: true
+  validates :prefectures_id,    presence: true
+  validates :send_day_id,       presence: true
+  validates :price,             presence: true
+  validates :image,             presence: true
 end
