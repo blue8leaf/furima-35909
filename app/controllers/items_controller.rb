255a8105057fa559fe6052d89d4e0cf8d610bff8
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-09-01 01:11:15 +0900

    テーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  
   def index
   end
 
@@ -7,5 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :status_id,:delivery_price_id, :prefectures_id, :send_day_id, :price).merge(user_id: current_user.id)
   end
 end
