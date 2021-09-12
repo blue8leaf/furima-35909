73054b7953c23a1ef36337435ba0c69cf5bd9872
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-09-12 19:34:59 +0900

    バリデーション

--- app/models/order_send.rb
+++ app/models/order_send.rb
@@ -1,4 +1,14 @@
 class OrderSend
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :municipalities, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :address
+    validates :telephone_number, format: {with: /\A\d{10,11}\z/}
+  end
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
 end
