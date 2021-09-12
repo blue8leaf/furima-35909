84b2bf5171eb7abc3bc43cbd59bc28fbff6956c9
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-09-12 19:47:05 +0900

    マイグレーションファイル

--- db/migrate/20210912104039_create_sends.rb
+++ db/migrate/20210912104039_create_sends.rb
@@ -1,7 +1,13 @@
 class CreateSends < ActiveRecord::Migration[6.0]
   def change
     create_table :sends do |t|
-
+      t.string     :postal_code,      null: false
+      t.integer    :prefecture_id,    null: false
+      t.string     :municipalities,   null: false
+      t.string     :address,          null: false
+      t.string     :building_name
+      t.string     :telephone_number, null: false
+      t.references :order,            null: false, foreign_key: true
       t.timestamps
     end
   end
