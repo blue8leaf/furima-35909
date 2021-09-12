bbd2b68862d05fcaafc7c419db979c46615b941d
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-09-12 19:38:35 +0900

    データをテーブルに保存

--- app/models/order_send.rb
+++ app/models/order_send.rb
@@ -11,4 +11,9 @@ class OrderSend
     validates :telephone_number, format: {with: /\A\d{10,11}\z/}
   end
   validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Send.create(postal_code: postal_code, prefecture_id: prefectures_id, municipalities: municipalities, address: address, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
+  end
 end
