f30b8bda627e3887a82484374df8d67630cf5da9
blue8leaf <86353443+blue8leaf@users.noreply.github.com>
2021-09-19 13:27:35 +0900

    rubocop4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-    if current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id
   end
 
   def create
@@ -23,12 +21,14 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefectures_id, :municipalities, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefectures_id, :municipalities, :house_number, :building_name, :telephone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
     pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe OrderAddress, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
-      sleep 0.1  
+      sleep 0.1
     end
 
     context '商品購入ができる場合' do
@@ -19,7 +19,7 @@ RSpec.describe OrderAddress, type: :model do
       end
     end
 
-    context '商品購入ができない場合' do 
+    context '商品購入ができない場合' do
       it '郵便番号がないと保存できない' do
         @order_address.postal_code = ''
         @order_address.valid?
@@ -28,7 +28,7 @@ RSpec.describe OrderAddress, type: :model do
       it '郵便番号は3桁ハイフン4桁の半角文字列のみでないと保存できない' do
         @order_address.postal_code = '1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid')
       end
       it '都道府県がないと保存できない' do
         @order_address.prefectures_id = ''
@@ -53,7 +53,7 @@ RSpec.describe OrderAddress, type: :model do
       it '電話番号は10桁以上11桁以内の半角数値のみでないと保存できない' do
         @order_address.telephone_number = '123456789'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Telephone number is invalid")
+        expect(@order_address.errors.full_messages).to include('Telephone number is invalid')
       end
       it 'userが紐付いていないと保存できないこと' do
         @order_address.user_id = nil
@@ -65,7 +65,7 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
-      it "tokenが空では登録できないこと" do
+      it 'tokenが空では登録できないこと' do
         @order_address.token = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
